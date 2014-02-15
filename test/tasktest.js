c6ee98d3e78dd1e53422ffc9403714260f683b47
hkbarton <hkbarton@gmail.com>
2014-02-14 17:58:23 -0800

    add product class

--- test/tasktest.js
+++ test/tasktest.js
@@ -1,4 +1,13 @@
-var task = require('../lib/droptask');
+var path = require('path');
 
-var testStep = new task.StepCopyFiles();
-testStep.execute();
+var prd = require('../lib/dropproduct'),
+    task = require('../lib/droptask');
+
+function testModelStructer(){
+  var nodeFolder = path.join(__dirname,'../');
+  var testProduct = new prd.Product('prd1', nodeFolder);
+  var task1 = new task.Task('task1', testProduct);
+  console.log(task1.getNodePath());
+}
+
+testModelStructer();
