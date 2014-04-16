663381e035fe2a8e087aa0657bd3bd6bfeb01600
hkbarton <hkbarton@gmail.com>
2014-04-16 15:45:26 -0700

    update file structure

--- test/objtreetest.js
+++ test/objtreetest.js
@@ -1,10 +1,11 @@
 var task = require('../lib/task.js'),
-    prd = require('../lib/product.js');
+    step = require('../lib/step.js'),
+    product = require('../lib/product.js');
 
-var testPrd = new prd.Product('product1', '/dropnode');
-var testTask = new task.Task('taskA', testPrd);
-var step1 = testTask.createStep(task.StepDeployFiles, null);
-var step2 = testTask.createStep(task.StepUpdateConfig, step1);
+var testPrd = new product('product1', '/dropnode');
+var testTask = new task('taskA', testPrd);
+var step1 = testTask.createStep(step.StepDeployFiles, null);
+var step2 = testTask.createStep(step.StepUpdateConfig, step1);
 
 console.log('Product: ' + testPrd.id + '  Path:' + testPrd.getNodePath());
 console.log('Task: ' + testTask.id + '  Path:' + testTask.getNodePath() + ' ContextProduct:' + testTask.context.product.id);
