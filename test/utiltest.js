a91f0c39c857f8bf3a37430f1afac4f9689db13d
hkbarton <hkbarton@gmail.com>
2014-06-06 11:42:58 -0700

    add some BDD test

--- test/utiltest.js
+++ test/utiltest.js
@@ -1,10 +1,20 @@
-var util = require('../lib/util');
+var assert = require('assert'),
+    util = require('../lib/util');
 
-console.log(util.getPrimaryIP());
+describe('util', function(){
+  it('should detect if a IP is or is not a private IP', function(){
+    assert.equal(util.isPrivateIP('10.0.0.1'), true);
+    assert.equal(util.isPrivateIP('11.0.0.1'), false);
+    assert.equal(util.isPrivateIP('172.17.0.1'), true);
+    assert.equal(util.isPrivateIP('172.32.0.1'), false);
+    assert.equal(util.isPrivateIP('192.168.0.1'), true);
+    assert.equal(util.isPrivateIP('192.169.0.1'), false);
+  });
 
-console.log(util.isPrivateIP('10.0.0.1'));
-console.log(util.isPrivateIP('11.0.0.1'));
-console.log(util.isPrivateIP('172.17.0.1'));
-console.log(util.isPrivateIP('172.32.0.1'));
-console.log(util.isPrivateIP('192.168.0.1'));
-console.log(util.isPrivateIP('192.169.0.1'));
+  it('should get primary internal IP', function(){
+    var ip = util.getPrimaryIP();
+    assert.equal((ip===null || ip.indexOf('192')===0 || 
+      ip.indexOf('172')===0 || 
+      ip.indexOf('10')===0), true);
+  });
+});
