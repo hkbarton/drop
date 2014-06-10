cddb9195ae5859d68e09710dec98e87df2088220
hkbarton <hkbarton@gmail.com>
2014-06-09 22:29:42 -0700

    finish deploy file test

--- test/step.js
+++ test/step.js
@@ -2,8 +2,7 @@ var path = require('path'),
     fs = require('fs'),
     assert = require('assert'),
     rimraf = require('rimraf'),
-    prd = require('../lib/product'),
-    task = require('../lib/task');
+    step = require('../lib/step');
 
 describe('task', function(){
   describe('developy file task', function(){
@@ -37,11 +36,65 @@ describe('task', function(){
       rimraf.sync(testFolder);
     });
 
-    it('should copy a folder recusively', function(){
-      var DeployStep = require('../lib/step').StepDeployFiles;
+    it('should copy a folder recusively', function(done){
+      var DeployStep = step.StepDeployFiles;
       var deploy = new DeployStep();
-      deploy.deployFile(testSrcFolder, testDestFolder, testBackupFolder);
-      // TODO need finsih this test
+      deploy.execute([testSrcFolder, testDestFolder, testBackupFolder], 
+      function(errs){
+        if (errs){
+          throw errs;
+        }
+        // check if copy succeed
+        var srcFileA = fs.readFileSync(
+          path.join(testSrcFolder, 'folder1', 'fileA.txt'),{encoding:'utf8'});
+        var destFileA = fs.readFileSync(
+          path.join(testDestFolder, 'folder1', 'fileA.txt'),{encoding:'utf8'});
+        assert.equal(srcFileA==destFileA, true);
+        var srcFileB = fs.readFileSync(
+          path.join(testSrcFolder, 'folder1', 'fileB.txt'),{encoding:'utf8'});
+        var destFileB = fs.readFileSync(
+          path.join(testDestFolder, 'folder1', 'fileB.txt'),{encoding:'utf8'});
+        assert.equal(srcFileB==destFileB, true);
+        var srcFileASub = fs.readFileSync(
+          path.join(testSrcFolder, 'folder1', 'subFolder1', 'fileA.txt'),
+          {encoding:'utf8'});
+        var destFileASub = fs.readFileSync(
+          path.join(testDestFolder, 'folder1', 'subFolder1', 'fileA.txt'),
+          {encoding:'utf8'});
+        assert.equal(srcFileASub==destFileASub, true);
+        var srcFileC = fs.readFileSync(
+          path.join(testSrcFolder, 'folder2', 'fileC.txt'),{encoding:'utf8'});
+        var destFileC = fs.readFileSync(
+          path.join(testDestFolder, 'folder2', 'fileC.txt'),{encoding:'utf8'});
+        assert.equal(srcFileC==destFileC, true);
+        var srcFile1 = fs.readFileSync(
+          path.join(testSrcFolder, 'file1.txt'),{encoding:'utf8'});
+        var destFile1 = fs.readFileSync(
+          path.join(testDestFolder, 'file1.txt'),{encoding:'utf8'});
+        assert.equal(srcFile1==destFile1, true);
+        var srcFile2 = fs.readFileSync(
+          path.join(testSrcFolder, 'file2.txt'),{encoding:'utf8'});
+        var destFile2 = fs.readFileSync(
+          path.join(testDestFolder, 'file2.txt'),{encoding:'utf8'});
+        assert.equal(srcFile2==destFile2, true);
+        // check if backup succeed
+        var backupIndex = fs.readFileSync(
+          path.join(testBackupFolder, '.index'),{encoding:'utf8'});
+        var backupFiles = backupIndex.split('\n');
+        for(var i in backupFiles){
+          if (backupFiles[i]){
+            var bfInfo = backupFiles[i].split('\001');
+            var bfContent = fs.readFileSync(
+              path.join(testBackupFolder, bfInfo[0]), {encoding:'utf8'});
+            if(bfInfo[1].indexOf('fileA')>-1){
+              assert(bfContent=='Old fileA in dest folder', true); 
+            }else if (bfInfo[1].indexOf('file2')>-1){
+              assert(bfContent=='old file2 in dest folder', true); 
+            }
+          }
+        }
+        done();
+      });
     });
   });
 });
